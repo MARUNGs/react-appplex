5fc076fbd22910d600a8cee465d30922e16203bd
MARUNGs <mi1764@naver.com>
2024-11-07 16:51:00 +0900

    노란줄 제거

--- src/screen/Search.tsx
+++ src/screen/Search.tsx
@@ -6,6 +6,9 @@ const Search = () => {
   // 내가 찾고자 하는 Query String Parameter를 조회하자.
   const search = new URLSearchParams(location.search);
   const keyword = search.get("keyword");
+  console.log(keyword);
+
+  // movie, tv list 중 검색키워드에 맞는 것만 보여주기
 
   return <></>;
 };
